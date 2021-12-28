dd65a58991150b44137eb080f3e06c8196ea7bf2
Eric Weise <eric.weise@chime.com>
2021-12-28 15:43:24 -0800

    added wind, other, temperature tables

--- get_requests.py
+++ get_requests.py
@@ -2,31 +2,13 @@
 import requests
 import json
 
-# This example uses Requests HTTP Library
-stationIds = ['F1405', 'F8022']
-
 with open('config.json') as json_data_file:
     config = json.load(json_data_file)
 
 API = config['API']['baseUrl']
 
 
-def observations(headers):
-    url = API + '/stations?state=CA&limit=10'
-
-    # Make get request to API with headers
+def station_observation(headers, station):
+    url = API + '/stations/' + station.id + '/observations/latest?require_qc=false'
     response = requests.get(url, headers=headers)
-    return response.text
-
-
-def stations(headers):
-    stations = []
-
-    for stationID in stationIds:
-        url = API + '/stations/' + stationID + '/observations/latest?require_qc=false'
-        response = requests.get(url, headers=headers)
-        stationDict = json.loads(response.text)
-        stations.append(stationDict)
-        # temperature = respDictionary["properties"]["temperature"]["value"]
-
-    return stations
+    return json.loads(response.text)
--- weather.py
+++ weather.py
@@ -3,12 +3,22 @@ from flask import render_template
 import os
 import get_requests
 import json
+from viewmodel import StationObservation
+from viewmodel import Station
+
 app = Flask(__name__)
 
 HEADERS = {
     'User-Agent': "(myweatherapp.com, contact@myweatherapp.com)",
     'accept': "application/geo+json",
 }
+
+floatFormat = "{:10.2f}"
+
+stations = [
+    Station('F1405', 'FW1405 San Anselmo'),
+    Station('F8022', 'FW8022 Pacifica')
+]
 
 
 @app.route("/")
@@ -17,42 +27,48 @@ def main():
     respDictionary = json.loads(jsonStr)
 
     stations = respDictionary["observationStations"]
-    return render_template('main.html', stations=stations)
+    return render_template('base.html', stations=stations)
+
 
 @app.route("/observations")
 def observations():
+    return render_template('table_temperature.html', observations=get_observations(), current='observations', table='temperature' )
 
-    stations = get_requests.stations(HEADERS)
 
-    responseStr = """
-    <table class="table">
-  <thead>
-    <tr>
-      <th scope="col">Station</th>
-      <th scope="col">Temperature</th>
-      <th scope="col">Dewpoint</th>
-      <th scope="col">barometric Pressure</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-    </tr>
-    """
+@app.route("/observations/wind")
+def wind():
+    return render_template('table_wind.html', observations=get_observations(), current='observations', table='wind')
 
+@app.route("/observations/other")
+def other():
+    return render_template('table_other.html', observations=get_observations(), current='observations', table='other')
+
+@app.route("/map")
+def map():
+    return render_template('map.html', current='map', )
+
+
+def get_observations():
+    obs = []
     for station in stations:
-        print(station)
-        temperature = station["properties"]["temperature"]["value"]
-        stationLink = station["properties"]["station"]
-        fahrenheit = (temperature * 9/5) + 32
-        responseStr += "<h1>Station " + stationLink + "  Temperature " + str(fahrenheit) + " </h1>"
-
-    responseStr += """  </tbody>
-</table>
-"""
-    return responseStr
-    
-
+        station_obs = get_requests.station_observation(HEADERS, station)
+        temp = station_obs["properties"]["temperature"]["value"]
+        fahrenheit = (temp * 9 / 5) + 32
+        obs.append(StationObservation(
+            station=station,
+            temperature=floatFormat.format(fahrenheit),
+            humidity=floatFormat.format(station_obs["properties"]["relativeHumidity"]["value"]),
+            dewpoint=station_obs["properties"]["dewpoint"]["value"],
+            wind_direction=station_obs["properties"]["windDirection"]["value"],
+            wind_speed=station_obs["properties"]["windSpeed"]["value"],
+            wind_gust=station_obs["properties"]["windGust"]["value"],
+            barometric_pressure=station_obs["properties"]["barometricPressure"]["value"],
+            sealevel_pressure=station_obs["properties"]["seaLevelPressure"]["value"],
+            precipitation_last3hours=station_obs["properties"]["precipitationLast3Hours"]["value"],
+            wind_chill=station_obs["properties"]["windChill"]["value"],
+            max_temperature_last24hours=station_obs["properties"]["maxTemperatureLast24Hours"]["value"],
+            min_temperature_last24hours=station_obs["properties"]["minTemperatureLast24Hours"]["value"],
+            visibility=station_obs["properties"]["visibility"]["value"],
+            heat_index=station_obs["properties"]["heatIndex"]["value"],
+        ))
+    return obs
